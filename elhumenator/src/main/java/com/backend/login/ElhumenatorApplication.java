802b97982e9355d452385ab2f6ab36f3eb27c1da
YogeeshYogi007 <yogeeshgowda476@gmail.com>
2022-12-12 14:34:46 +0530

    Elhumenator

---
+++ elhumenator/src/main/java/com/backend/login/ElhumenatorApplication.java
@@ -0,0 +1,13 @@
+package com.backend.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElhumenatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElhumenatorApplication.class, args);
+	}
+
+}
